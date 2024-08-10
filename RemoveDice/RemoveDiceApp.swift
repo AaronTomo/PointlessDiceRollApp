034b336b5b75e852fdccd0df4230a5957aaadc45
Aaron Thompson <aaronseanthompson@icloud.com>
2024-08-10 11:42:27 +0100

    Initial Commit

---
+++ RemoveDice/RemoveDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoveDiceApp.swift
+//  RemoveDice
+//
+//  Created by Aaron Thompson on 10/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RemoveDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
