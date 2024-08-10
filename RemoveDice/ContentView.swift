bab67c4fd2b41c772589da84d9f9be3eb85dfb11
Aaron Thompson <aaronseanthompson@icloud.com>
2024-08-10 21:34:07 +0100

    Init

--- RemoveDice/ContentView.swift
+++ RemoveDice/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var rolledNumber = 0
+    @State var remainingNumbers = [1, 2, 3, 4, 5, 6]
+    @State var selectedNumbers = [Int]()
+    @State var presAlert = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+            VStack {
+                Text("\(rolledNumber)").font(.largeTitle)
+                    .padding()
+                Button("Roll", action: rollDice)
+            }
+            .frame(width: 300, height: 100)
+            
+           
+    
+        NavigationStack{
+            List {
+                ForEach(selectedNumbers, id: \.self){ num in
+                    Text("\(num)")
+                }
+            }
+            .navigationTitle("Previous Numbers")
+            .animation(.default)
         }
-        .padding()
+        .alert("All sides rolled." ,isPresented: $presAlert){
+            Button("Restart", action: restartRolls)
+        }
+        
+        
+    }
+    
+    func rollDice() {
+        rolledNumber = remainingNumbers.randomElement() ?? 0
+        
+        if(rolledNumber == 0){
+            presAlert = true
+            return
+        }
+        selectedNumbers.append(rolledNumber)
+        remainingNumbers = remainingNumbers.filter {
+            $0 != rolledNumber
+        }
+        
+    }
+    
+    func restartRolls() {
+        rolledNumber = 0
+        remainingNumbers = [1, 2, 3, 4, 5, 6]
+        selectedNumbers = [Int]()
+        presAlert = false
     }
 }
 
